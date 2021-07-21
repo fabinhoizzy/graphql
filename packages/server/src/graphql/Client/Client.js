92d34bacc1511d33509f8c4b3038fd4d4263433e
fabinhoizzy <fabinhoevil16@hotmail.com>
2021-07-20 21:00:39 -0300

    Criando GraphQL Queries

--- packages/server/src/graphql/Client/Client.js
+++ packages/server/src/graphql/Client/Client.js
@@ -1,8 +1,27 @@
 import { gql } from 'apollo-server-express';
+import createRepository from '../../data/io/Database/createRepository';
+
+const clientRepository = createRepository('client');    
 
 export const typeDefs = gql`
-    type Client{
+    type Client implements Node{
         id: ID!
         name: String!
+        email: String!
+        disabled: Boolean!
+    }
+
+    extend type Query {
+        client(id: ID!):client
+        # clients: [Client!]!
     }
 `;
+
+export const resolvers = {
+    Query: {
+        client: async (_,{ id }) => {
+            const clients = await clientRepository.read();
+            return clients.find((client) => client.id === id);
+        },    
+    },
+};
